40fb029e2ab67cbc310ee86e04accee71aedc35e
kyleskom <kyleskom@buffalo.edu>
2019-12-29 18:03:14 -0500

    Added base for tensorflow model

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import pandas as pd
 import os
 import numpy as np
 
-# directory = os.fsdecode('Odds-Data-Clean')
-# df = pd.read_excel(directory + '/2007-08-Clean.xlsx')
+data = pd.read_excel('2007-2008-Games.xlsx')
+scores = data['Score']
+data.drop(['Score'], axis=1, inplace=True)
 
-for i in range(1000):
-    print('TEST', i)
+data = data.drop(columns=['Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1'])
+data = data.values
+data = data.astype(float)
+
+x_train = tf.keras.utils.normalize(data, axis=1)
+y_train = np.asarray(scores)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(299, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=100)
+print('DONE')
