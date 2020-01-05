1cc1ed7b9443134e798f7d657dbdf184078775c0
kyleskom <kyleskom@buffalo.edu>
2020-01-05 00:24:36 -0500

    Use relu6, gives better results. New best model

--- Train_Model.py
+++ Train_Model.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
 import time
 
-tensorboard = TensorBoard(log_dir='Logs/{}'.format(str(time.time())))
+
+t = str(time.time())
+tensorboard = TensorBoard(log_dir='Logs/{}'.format(t))
+earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+mcp_save = ModelCheckpoint('Trained-Model-' + t, save_best_only=True, monitor='val_loss', mode='min')
 
 data = pd.read_excel('Full-Data-Set.xlsx')
 scores = data['Score']
@@ -21,15 +25,14 @@ y_train = np.asarray(margin)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
-# model.add(tf.keras.layers.Dense(512, activation=tf.nn.relu))
-# model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(512, activation=tf.nn.relu6))
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu6))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu6))
 model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=20, validation_split=0.1)
-# model.fit(x_train, y_train, epochs=20, validation_split=0.1, callbacks=[tensorboard])
-
+#history = model.fit(x_train, y_train, epochs=20, validation_split=0.1)
+model.fit(x_train, y_train, epochs=50, validation_split=0.1, batch_size=32, callbacks=[tensorboard, earlyStopping, mcp_save])
 
 
 #
@@ -37,6 +40,8 @@ model.fit(x_train, y_train, epochs=20, validation_split=0.1)
 # print(val_loss)
 # print(val_acc)
 
-#model.save('Trained-Model')
+#print(history.history)
+
+#model.save('Trained-Model-' + str(history.history['val_loss']))
 
 print('DONE')
