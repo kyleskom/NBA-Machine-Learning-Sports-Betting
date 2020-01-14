98dc8c0e94b81673c49c69a6df020c1bcdf7f6de
kyleskom <kyleskom@buffalo.edu>
2020-01-13 19:07:55 -0500

    Cleaned up U/O branch

--- Test_Current_Season_OU.py
+++ Test_Current_Season_OU.py
@@ -3,11 +3,11 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-model = load_model('Trained-Model')
-data = pd.read_excel('Full-Data-Set.xlsx')
+model = load_model('Trained-Model-OU')
+data = pd.read_excel('Full-Data-Set-UnderOver.xlsx')
 
 data = data.iloc[15068:]
-data.drop(['Score', 'Home-Team-Win', 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1'], axis=1, inplace=True)
+data.drop(['Score', 'Home-Team-Win', 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU-Cover'], axis=1, inplace=True)
 
 data = data.values
 data = data.astype(float)
