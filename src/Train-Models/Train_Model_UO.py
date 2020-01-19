f4df09dc252b800a82260803b477b62bbd07708e
kyleskom <kyleskom@buffalo.edu>
2020-01-19 14:09:18 -0500

    Update paths models save to on train

--- src/Train-Models/Train_Model_UO.py
+++ src/Train-Models/Train_Model_UO.py
@@ -8,7 +8,7 @@ current_time = str(time.time())
 
 tensorboard = TensorBoard(log_dir='Logs/{}'.format(current_time))
 earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
-mcp_save = ModelCheckpoint('Trained-Model-ML-' + current_time, save_best_only=True, monitor='val_loss', mode='min')
+mcp_save = ModelCheckpoint('Models/Trained-Model-ML-' + current_time, save_best_only=True, monitor='val_loss', mode='min')
 
 data = pd.read_excel('Datasets/Full-Data-Set-UnderOver.xlsx')
 OU = data['OU-Cover']
