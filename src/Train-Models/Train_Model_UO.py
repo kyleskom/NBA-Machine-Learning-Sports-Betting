8834ec62cb092ab014fbb1172645b3b97e539473
Kyle skompinski <kyleskompinski@Kyles-MacBook-Pro.local>
2021-01-19 13:11:46 -0500

    Test training for new season

--- src/Train-Models/Train_Model_UO.py
+++ src/Train-Models/Train_Model_UO.py
@@ -8,9 +8,9 @@ current_time = str(time.time())
 
 tensorboard = TensorBoard(log_dir='Logs/{}'.format(current_time))
 earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
-mcp_save = ModelCheckpoint('Models/Trained-Model-ML-' + current_time, save_best_only=True, monitor='val_loss', mode='min')
+mcp_save = ModelCheckpoint('../../Models/Trained-Model-ML-' + current_time, save_best_only=True, monitor='val_loss', mode='min')
 
-data = pd.read_excel('Datasets/Full-Data-Set-UnderOver.xlsx')
+data = pd.read_excel('../../Datasets/Full-Data-Set-UnderOver2.xlsx')
 OU = data['OU-Cover']
 data.drop(['Score', 'Home-Team-Win', 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU-Cover'], axis=1, inplace=True)
 
