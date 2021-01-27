efe4e5e5a45283cb7873c67786d129852821d997
Kyle Skompinski <kyleskom@buffalo.edu>
2021-01-27 13:39:30 -0500

    Renamed xgboost class

--- src/Train-Models/XGBoost_Model.py
+++ src/Train-Models/XGBoost_Model.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 
 data = pd.read_excel('../../Datasets/TEST123.xlsx')
 scores = data['Score']
-margin = data['Home-Team-Win']
-data.drop(['Score', 'Home-Team-Win', 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU', 'OU-Cover'], axis=1, inplace=True)
+OU = data['OU-Cover']
+data.drop(['Score', 'Home-Team-Win', 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU-Cover'], axis=1, inplace=True)
 data = data.values
 data = data.astype(float)
 
-x_train, x_test, y_train, y_test = train_test_split(data, margin, test_size=.1)
+x_train, x_test, y_train, y_test = train_test_split(data, OU, test_size=.1)
 
 train = xgb.DMatrix(x_train, label=y_train)
 test = xgb.DMatrix(x_test, label=y_test)
 
 param = {
-    'max_depth': 3,
+    'max_depth': 7,
     'eta': 0.1,
     'objective': 'multi:softmax',
-    'num_class': 2
+    'num_class': 3
 }
 epochs = 100
 
