0e8ba4cfd618701820da72f0716663a7a04595ad
Kyle Skompinski <kyleskom@buffalo.edu>
2021-10-22 11:17:25 -0400

    Updated for 2021-22 season

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ data_url = 'https://stats.nba.com/stats/leaguedashteamstats?' \
            'MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&' \
            'PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&' \
            'PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&' \
-           'Season=2020-21&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&' \
+           'Season=2021-22&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&' \
            'StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision='
 
 
