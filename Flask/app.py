4dea9859184742b1a04af7fd0de5fd8f0a7e7413
Nolan Gilley <nkgilley@gmail.com>
2023-01-20 12:08:24 -0500

    update table format

--- Flask/app.py
+++ Flask/app.py
@@ -1,3 +1,4 @@
+from datetime import date
 from flask import Flask, render_template
 from functools import lru_cache
 import subprocess
@@ -23,8 +24,9 @@ def fetch_betmgm(ttl_hash=None):
 def fetch_game_data(sportsbook="fanduel"):
     cmd = ["python", "main.py", "-xgb", f"-odds={sportsbook}"]
     stdout = subprocess.check_output(cmd, cwd="../").decode()
-    data_re = re.compile(r'\n(?P<away_team>[\w ]+)(\((?P<away_confidence>[\d+\.]+)%\))? vs (?P<home_team>[\w ]+)(\((?P<home_confidence>[\d+\.]+)%\))?: (?P<ou_pick>OVER|UNDER) (?P<ou_value>[\d+\.]+) (\((?P<ou_confidence>[\d+\.]+)%\))?', re.MULTILINE)
+    data_re = re.compile(r'\n(?P<home_team>[\w ]+)(\((?P<home_confidence>[\d+\.]+)%\))? vs (?P<away_team>[\w ]+)(\((?P<away_confidence>[\d+\.]+)%\))?: (?P<ou_pick>OVER|UNDER) (?P<ou_value>[\d+\.]+) (\((?P<ou_confidence>[\d+\.]+)%\))?', re.MULTILINE)
     ev_re = re.compile(r'(?P<team>[\w ]+) EV: (?P<ev>[-\d+\.]+)', re.MULTILINE)
+    odds_re = re.compile(r'(?P<away_team>[\w ]+) \((?P<away_team_odds>-?\d+)\) @ (?P<home_team>[\w ]+) \((?P<home_team_odds>-?\d+)\)', re.MULTILINE)
     games = []
     for match in data_re.finditer(stdout):
         game_dict = {'away_team': match.group('away_team').strip(),
@@ -35,11 +37,16 @@ def fetch_game_data(sportsbook="fanduel"):
                      'ou_value': match.group('ou_value'),
                      'ou_confidence': match.group('ou_confidence')}
         for ev_match in ev_re.finditer(stdout):
-            print(ev_match.group('team'), game_dict['home_team'])
             if ev_match.group('team') == game_dict['away_team']:
                 game_dict['away_team_ev'] = ev_match.group('ev')
             if ev_match.group('team') == game_dict['home_team']:
                 game_dict['home_team_ev'] = ev_match.group('ev')
+        for odds_match in odds_re.finditer(stdout):
+            if odds_match.group('away_team') == game_dict['away_team']:
+                game_dict['away_team_odds'] = odds_match.group('away_team_odds')
+            if odds_match.group('home_team') == game_dict['home_team']:
+                game_dict['home_team_odds'] = odds_match.group('home_team_odds')
+
         print(game_dict)
         games.append(game_dict)
     return games
@@ -51,10 +58,13 @@ def get_ttl_hash(seconds=600):
 
 
 app = Flask(__name__)
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
 
 @app.route("/")
 def index():
     fanduel = fetch_fanduel(ttl_hash=get_ttl_hash())
     draftkings = fetch_draftkings(ttl_hash=get_ttl_hash())
     betmgm = fetch_betmgm(ttl_hash=get_ttl_hash())
-    return render_template('index.html', data={"fanduel": fanduel, "draftkings": draftkings, "betmgm": betmgm})
+
+    return render_template('index.html', today=date.today(), data={"fanduel": fanduel, "draftkings": draftkings, "betmgm": betmgm})
