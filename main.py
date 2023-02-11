3cafdaae68d7850ca750a9627f9e0e519912b053
Kyle Skompinski <kyleskom@buffalo.edu>
2023-02-10 19:28:07 -0500

    Fixed dropped columns

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def createTodaysGames(games, df, odds):
     games_data_frame = pd.concat(match_data, ignore_index=True, axis=1)
     games_data_frame = games_data_frame.T
 
-    frame_ml = games_data_frame.drop(columns=['TEAM_ID', 'CFID', 'CFPARAMS', 'TEAM_NAME'])
+    frame_ml = games_data_frame.drop(columns=['TEAM_ID', 'TEAM_NAME'])
     data = frame_ml.values
     data = data.astype(float)
 
