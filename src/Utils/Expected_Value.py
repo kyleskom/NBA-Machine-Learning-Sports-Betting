bf046ee69d5d6518d424244885f271958c4b6b47
AaronAshery <aaron.ashery1@gmail.com>
2023-04-26 14:28:48 -0400

    Added Wager Suggestions based on the Kelly Criterion
    Currently the model ignores EV to pick its winner.
    People want to use this tool to place wagers, and smart wagering is based on expected value.
    Kelly Criterion suggests an amount to wager taking into account the EV as well as the odds.

--- src/Utils/Expected_Value.py
+++ src/Utils/Expected_Value.py
@@ -9,3 +9,16 @@ def payout(odds):
         return odds
     else:
         return (100 / (-1 * odds)) * 100
+
+def k_odds(american_odds):
+    if american_odds > 0:
+        return (american_odds/100)
+    else:
+        return -1 * (100/american_odds)
+
+def kelly(Pwin, american_odds, bankroll=100, adjustment=1.0):
+    odds = k_odds(american_odds)
+    f = Pwin - ((1 - Pwin)/odds)
+    wager = (f * bankroll) / adjustment
+    wager = 0 if wager < 0 else wager
+    return round(wager,2)
