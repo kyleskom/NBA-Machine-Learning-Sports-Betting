cd6cb22f6f05ee6e2d45b495bd4c9bf6cd8a63c8
Kyle Skompinski <kyleskom@buffalo.edu>
2023-10-27 12:26:42 -0400

    Update daily stats for 2023-24

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ data_url = 'https://stats.nba.com/stats/leaguedashteamstats?' \
            'MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&' \
            'PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&' \
            'PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&' \
-           'Season=2022-23&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&' \
+           'Season=2023-24&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&' \
            'StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision='
 
 
