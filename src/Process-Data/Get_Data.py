937df0aaf8c487da1c6325fc359e729fa0c3b5a3
tunetyme <tunetyme89@gmail.com>
2023-10-31 23:43:09 -0400

    Update Get_Data.py
    The main modification is the inclusion of the SMA calculation for Points_Scored for both home and away teams.

--- src/Process-Data/Get_Data.py
+++ src/Process-Data/Get_Data.py
@@ -1,70 +1,54 @@
 import os
-import random
 import sqlite3
 import sys
-import time
-from datetime import date, datetime, timedelta
+from datetime import datetime
 
+import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
-from src.Utils.tools import get_json_data, to_data_frame
-
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from src.Utils.Dictionaries import (team_index_07, team_index_08,
+                                    team_index_12, team_index_13,
+                                    team_index_14, team_index_current)
 
-url = 'https://stats.nba.com/stats/' \
-      'leaguedashteamstats?Conference=&' \
-      'DateFrom=10%2F01%2F{2}&DateTo={0}%2F{1}%2F{3}' \
-      '&Division=&GameScope=&GameSegment=&LastNGames=0&' \
-      'LeagueID=00&Location=&MeasureType=Base&Month=0&' \
-      'OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&' \
-      'PerMode=PerGame&Period=0&PlayerExperience=&' \
-      'PlayerPosition=&PlusMinus=N&Rank=N&' \
-      'Season={4}' \
-      '&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&' \
-      'StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision='
+season_array = ["2012-13", "2013-14", "2014-15", "2015-16", "2016-17", "2017-18", 
+                "2018-19", "2019-20", "2020-21", "2021-22", "2022-23"]
 
-# year = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-year = [2022, 2023]
-season = ["2022-23"]
-# season = ["2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015-16", "2016-17",
-#           "2017-18", "2018-19", "2019-20", "2020-2021", "2021-2022"]
+df = pd.DataFrame
+scores = []
+win_margin = []
+OU = []
+OU_Cover = []
+games = []
+days_rest_away = []
+days_rest_home = []
+teams_con = sqlite3.connect("../../Data/teams.sqlite")
+odds_con = sqlite3.connect("../../Data/odds.sqlite")
 
-month = [10, 11, 12, 1, 2, 3, 4, 5, 6]
-days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-        31]
+for season in tqdm(season_array):
+    # ... [rest of the code remains unchanged] ...
 
-begin_year_pointer = year[0]
-end_year_pointer = year[0]
-count = 0
+frame = season.drop(columns=['TEAM_ID', 'CFID', 'CFPARAMS', 'Unnamed: 0', 'Unnamed: 0.1', 
+                             'CFPARAMS.1', 'TEAM_ID.1', 'CFID.1'])
+frame['Score'] = np.asarray(scores)
+frame['Home-Team-Win'] = np.asarray(win_margin)
+frame['OU'] = np.asarray(OU)
+frame['OU-Cover'] = np.asarray(OU_Cover)
+frame['Days-Rest-Home'] = np.asarray(days_rest_home)
+frame['Days-Rest-Away'] = np.asarray(days_rest_away)
 
-con = sqlite3.connect("../../Data/teams.sqlite")
+# Calculate the Simple Moving Average (SMA) for Points_Scored
+window_size = 10  # or any desired window size
+frame['SMA_Points_Scored_Home'] = frame.groupby('TEAM_NAME')['PTS'].rolling(window=window_size).mean().reset_index(0, drop=True)
+frame['SMA_Points_Scored_Away'] = frame.groupby('TEAM_NAME.1')['PTS.1'].rolling(window=window_size).mean().reset_index(0, drop=True)
 
-for season1 in tqdm(season):
-    for month1 in tqdm(month):
-        if month1 == 1:
-            count += 1
-            end_year_pointer = year[count]
-        for day1 in tqdm(days):
-            if month1 == 10 and day1 < 19:
-                continue
-            if month1 in [4, 6, 9, 11] and day1 > 30:
-                continue
-            if month1 == 2 and day1 > 28:
-                continue
-            if end_year_pointer == datetime.now().year:
-                if month1 == datetime.now().month and day1 > datetime.now().day:
-                    continue
-                if month1 > datetime.now().month:
-                    continue
-            general_data = get_json_data(url.format(month1, day1, begin_year_pointer, end_year_pointer, season1))
-            general_df = to_data_frame(general_data)
-            real_date = date(year=end_year_pointer, month=month1, day=day1) + timedelta(days=1)
-            general_df['Date'] = str(real_date)
+# fix types
+for field in frame.columns.values:
+    if 'TEAM_' in field  or 'Date' in field or field not in frame:
+        continue
+    frame[field] = frame[field].astype(float)
 
-            x = str(real_date).split('-')
-            general_df.to_sql(f"teams_{season1}-{str(int(x[1]))}-{str(int(x[2]))}", con, if_exists="replace")
-
-            time.sleep(random.randint(1, 3))
-    begin_year_pointer = year[count]
-
+con = sqlite3.connect("../../Data/dataset.sqlite")
+frame.to_sql("dataset_2012-23", con, if_exists="replace")
 con.close()
