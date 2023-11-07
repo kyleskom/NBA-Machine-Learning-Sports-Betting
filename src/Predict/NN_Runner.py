ad8c713621acb3de0ad58081c3afe46ddb14a4b0
Nolan Gilley <nkgilley@gmail.com>
2023-11-06 19:08:03 -0500

    update nn models

--- src/Predict/NN_Runner.py
+++ src/Predict/NN_Runner.py
@@ -9,8 +9,8 @@ from src.Utils import Expected_Value
 from src.Utils import Kelly_Criterion as kc
 
 init()
-model = load_model('Models/NN_Models/Trained-Model-ML-1680133120.689445')
-ou_model = load_model("Models/NN_Models/Trained-Model-OU-1680133008.6887271")
+model = load_model('Models/NN_Models/Trained-Model-ML-1699315388.285516')
+ou_model = load_model("Models/NN_Models/Trained-Model-OU-1699315414.2268295")
 
 
 def nn_runner(data, todays_games_uo, frame_ml, games, home_team_odds, away_team_odds, kelly_criterion):
